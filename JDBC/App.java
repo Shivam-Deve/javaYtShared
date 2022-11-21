54e443804ed7090a0f9ebd626a3c555cfbe1ad20
Shivam-Deve <67728502+Shivam-Deve@users.noreply.github.com>
2022-11-21 21:01:41 +0530

    Update App.java

--- JDBC/App.java
+++ JDBC/App.java
@@ -2,6 +2,8 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // Class.forName("com.mysql.jdbc.Driver"); // load and register driver | not needed in current versions
+        
         // connect to database
         String url = "jdbc:mysql://localhost:3306/jdbc";
         String userName = "root";
